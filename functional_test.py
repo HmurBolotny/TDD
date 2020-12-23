1a3f64c25f7184c2d292238fde7e3dcfef3f7f6d
HmurBolotny <djtestch98@gmail.com>
2020-12-23 13:06:39 +0500

    использование unitest описание минимальноработоспособного приложения

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''test new visiter'''
+    def setUp(self):
+        '''install'''
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        '''uninstall'''
+        self.browser.quit()
+
+    def test_can_start_a_list_retrieve_it_later(self):
+        ''''''
+        # проверка домашней страници
+        self.browser.get('http://localhost:8000')
+        # проверка заголовка и шапки о списке дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('end test')
+        # предлагаеться добавить элемент списка
+        # ввести задание
+        # после воода страница обновляется и содержит задание в качестве элемента страници
+        # тектовое поле предлагает ввести задание
+        # воодиться здание 2
+        # страница обновляется и содержит  оба элемента
+        # проверка сохранения списка
+        # генерируеться уникальный url-адрес и выводится текст с пояснением
+        # преход по указанному адресу - проверка списка заданий
+        # проверка окончена заввершение работы приложения
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
